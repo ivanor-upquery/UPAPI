5a07f633f2b93c4343de9154310c641a7aeb25f3
ivanor-upquery <ivanor@uquery.com>
2023-07-11 16:42:55 -0300

    teste

--- etl/teste_ct.py
+++ etl/teste_ct.py
@@ -27,30 +27,42 @@ import os
 import csv
 import fnmatch
 
-print('a0')
+print('inicio : ' + str(datetime.today()) )
 
-dsn_origem = cx_Oracle.makedsn('capital-rds.conexos.cloud', '1521', 'CONEXOS')
-con = cx_Oracle.connect(user='CNXBI_CAPITAL',password='CZCBABSDUYSADVCAVG', dsn=dsn_origem)
+
+fonte_host = 'cloud.upquery.com'
+fonte_user = 'dwu'
+fonte_pass = 'S7hHmdmz28i2'
+fonte_serv = 'capital'
+fonte_port = '1521'
+
+dsn = cx_Oracle.makedsn(fonte_host,port=fonte_port,service_name=fonte_serv)
+engine = create_engine('oracle+cx_oracle://%s:%s@%s' % (fonte_user, fonte_pass, dsn))
+engine.fast_executemany = True
 
 exec_comando = "SELECT PRI_ESP_REFCLIENTE, FIL_COD, PRD_COD, TEC_ESP_COD, TEC_ESP_COD_SEQ, PRD_DES_NOME, INI_PCT_II, INI_PCT_IPI, INI_PCT_COFINS, INI_PCT_PIS, INI_PCT_ICMS,PRI_COD FROM VB_INVOICE_ITENS"
 exec_clear   = "DELETE FROM TESTE_ETL_INVOICE_ITENS"
 colunas      = ["PRI_ESP_REFCLIENTE","FIL_COD","PRD_COD","TEC_ESP_COD","TEC_ESP_COD_SEQ","PRD_DES_NOME","INI_PCT_II","INI_PCT_IPI","INI_PCT_COFINS","INI_PCT_PIS","INI_PCT_ICMS","PRI_COD"]
 
-print('buscando - sql: ' +  + str(datetime.today()) )
+print('buscando - sql: ' + str(datetime.today()) )
+dsn_origem = cx_Oracle.makedsn('capital-rds.conexos.cloud', '1521', 'CONEXOS')
+con = cx_Oracle.connect(user='CNXBI_CAPITAL',password='CZCBABSDUYSADVCAVG', dsn=dsn_origem)
 dados = pd.read_sql(exec_comando, con)
-print('formatando: ' +  + str(datetime.today()) )
+con.close 
+
+print('formatando: ' + str(datetime.today()) )
 dados = pd.DataFrame(dados)
 dados.columns = colunas
 dados.columns = dados.columns.str.strip().str.lower()
 object_columns = [c for c in dados.columns[dados.dtypes == 'object'].tolist()]
 dtyp = {c:sa.types.VARCHAR(dados[c].astype('str').str.len().max()) for c in object_columns}
-engine = create_engine('oracle+cx_oracle://%s:%s@%s' % (fonte_user, fonte_pass, dsn))
-print('inserindo : ' +  + str(datetime.today()) )
+
+print('inserindo : ' + str(datetime.today()) )
 with engine.connect() as con0:
 	if exec_clear is not None:
 		r_del = con0.execute(exec_clear)
 	dados.to_sql(name='TESTE_ETL_INVOICE_ITENS',con=con0, if_exists='append', index=False, chunksize=50000,  dtype=dtyp)
 	
-print('fim : ' +  + str(datetime.today()) )
+print('fim : ' + str(datetime.today()) )
 
 
